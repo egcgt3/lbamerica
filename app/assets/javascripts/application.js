bc3a9acdc50b665c7c7b79c04d8ec11a46b91dbc
Gerry <egcgt3@gmail.com>
2013-03-21 14:21:20 -0700

    Initial Commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,37 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
+// GO AFTER THE REQUIRES BELOW.
+//
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+$(function() {
+    $('ul a').bind('click',function(event){
+        var $anchor = $(this);
+ 
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500,'easeInOutExpo');
+
+
+        $('li a').removeClass("button orange");
+
+        $(this).addClass("button orange");
+
+        /*
+        if you don't want to use the easing effects:
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000);
+        */
+        event.preventDefault();
+    });
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lbamerica::Application.config.secret_token = 'e9c6e5adb339d5d8185d6e345101261fd54bc08b6d68754e5cc89e3df9a91b9299f9b20ac0a8c82daedf586f9d1cd455e1a3df99ba84c47c32af5f841fd58c85'
